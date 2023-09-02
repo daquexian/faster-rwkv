b91fd9d15ad2d355c730ec24c1458df7f259a195
daquexian <daquexian566@gmail.com>
2023-09-02 19:09:51 +0000

    add an example file
    Signed-off-by: daquexian <daquexian566@gmail.com>

---
+++ android-example/MainActivity.java
@@ -0,0 +1,86 @@
+// NOTE: Only for reference
+
+package com.rwkv.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.ColorSpace;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.TextView;
+import com.rwkv.faster.*;
+
+import com.rwkv.demo.databinding.ActivityMainBinding;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'faster' library on application startup.
+    static {
+        System.loadLibrary("faster_rwkv_jni");
+    }
+
+    private ActivityMainBinding binding;
+    private Model model;
+    private ABCTokenizer tokenizer;
+    private Sampler sampler;
+
+    private static int[] add2BeginningOfArray(int[] elements, int element)
+    {
+        int[] newArray = Arrays.copyOf(elements, elements.length + 1);
+        newArray[0] = element;
+        System.arraycopy(elements, 0, newArray, 1, elements.length);
+
+        return newArray;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Environment.isExternalStorageManager()) {
+            //todo when permission is granted
+        } else {
+            //request for the permission
+            Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+            Uri uri = Uri.fromParts("package", getPackageName(), null);
+            intent.setData(uri);
+            startActivity(intent);
+        }
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        String input = "S:2";
+        String result = input;
+
+        model = new Model("/sdcard/Download/rwkv/RWKV-4-ABC-82M-v1-20230805-ctx1024-ncnn", "ncnn fp32");
+        tokenizer = new ABCTokenizer();
+        sampler = new Sampler();
+
+        int BOS_ID = 0;
+        int EOS_ID = 0;
+        float temperature = 1;
+        int top_k = 1;
+        float top_p = 0.0f;
+
+        int[] input_ids = tokenizer.encode(input);
+        input_ids = add2BeginningOfArray(input_ids, BOS_ID);
+        float[] logits = model.run(input_ids);
+        for (int i = 0; i < 1024; i++) {
+            int output_id = sampler.sample(logits, temperature, top_k, top_p);
+            if (output_id == EOS_ID) {
+                break;
+            }
+            String output = tokenizer.decode(output_id);
+            result += output;
+            logits = model.run(output_id);
+        }
+        Log.i("xxx", result);
+    }
+}
