c70d022aae3f3382c125bc1ea955f4cd94f34c7f
daquexian <daquexian566@gmail.com>
2023-09-04 11:21:37 +0000

    remove unused ncnn ops
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- test_ops.cpp
+++ test_ops.cpp
@@ -5,11 +5,13 @@
 
 // TODO: add more op tests
 
-TEST(RWKV, cpu_scalar_div_fp32) {
-  auto x = rwkv::Tensor::Empty({256}, rwkv::DType::kFloat32, rwkv::Device::kCUDA);
+#ifdef FR_ENABLE_CUDA
+TEST(RWKV, cuda_scalar_div_fp16) {
+  auto x = rwkv::Tensor::Empty({256}, rwkv::DType::kFloat16, rwkv::Device::kCUDA);
   rwkv::fill_(x, 1.0f);
-  x = rwkv::Copy(x, rwkv::Device::kCPU);
   rwkv::scalar_div_(x, 2.0f);
+  x = rwkv::cast_dtype(rwkv::Copy(x, rwkv::Device::kCPU), rwkv::DType::kFloat32);
   auto x_ptr = x.data_ptr<float>();
   EXPECT_EQ(x_ptr[0], 0.5f);
 }
+#endif
