365871e5004db91ca159f1df14d70861ac380f81
daquexian <daquexian566@gmail.com>
2023-09-30 16:40:50 +0000

    ppl test: remove the dependency of pytorch, support custom size
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- tests/lambada_ppl.py
+++ tests/lambada_ppl.py
@@ -2,41 +2,21 @@
 # The RWKV Language Model - https://github.com/BlinkDL/RWKV-LM
 ########################################################################################################
 
-import os, sys, types, json, math, time
+import json
+import math
+import os
+import sys
 import numpy as np
 np.set_printoptions(precision=4, suppress=True, linewidth=200)
 with open("/data/user/cangshui/tianchao/repos/ChatRWKV/misc/lambada_test.jsonl", "r", encoding="utf-8") as f:
     todo = [json.loads(line) for line in f]
     todo = [[doc['text'].rsplit(' ', 1)[0], " " + doc['text'].rsplit(' ', 1)[1]] for doc in todo]
 
-########################################################################################################
+todo = todo[:int(os.getenv("FR_LAMBADA_SIZE", 99999999999))]
 
-# MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-14b/RWKV-4-Pile-14B-20230213-8019'
-# MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-1b5/RWKV-4-Pile-1B5-20220903-8040'
-# MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-7b/RWKV-4-Pile-7B-20230109-ctx4096'
-# MODEL_NAME = '/data/user/cangshui/tianchao/pth_models/RWKV-4-World-CHNtuned-1.5B-v1-20230620-ctx4096.pth'
-# MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-169m/RWKV-4-Pile-169M-20220807-8023'
+TOKENIZER_NAME = sys.argv[1]
+MODEL_NAME = sys.argv[2]
 
-PAD_SEQ = []
-
-########################################################################################################
-
-print(f'\nLoading ChatRWKV https://github.com/BlinkDL/ChatRWKV')
-import torch
-
-# Tune these below (test True/False for all of them) to find the fastest setting:
-# torch._C._jit_set_profiling_executor(True)
-# torch._C._jit_set_profiling_mode(True)
-# torch._C._jit_override_can_fuse_on_cpu(True)
-# torch._C._jit_override_can_fuse_on_gpu(True)
-# torch._C._jit_set_texpr_fuser_enabled(False)
-# torch._C._jit_set_nvfuser_enabled(False)
-
-from torch.nn import functional as F
-
-MODEL_NAME = '/data/user/cangshui/tianchao/repos/fr-models/RWKV-4-World-0.1B-v1-20230520-ctx4096/native-fr/RWKV-4-World-0.1B-v1-20230520-ctx4096.fr'
-TOKENIZER_NAME = '/data/user/cangshui/tianchao/repos/fr-models/RWKV-4-World-0.1B-v1-20230520-ctx4096/native-fr/'
-print(f'Loading model - {MODEL_NAME}')
 import fasterrwkv as fr
 model = fr.Model(MODEL_NAME, 'cuda fp16')
 sampler = fr.Tokenizer(TOKENIZER_NAME)
@@ -46,7 +26,13 @@ def forward_with_full_output(model, input_ids):
     for i in range(len(input_ids)):
         output = model(input_ids[i])
         outputs.append(output)
-    return torch.tensor(np.array(outputs))
+    return np.array(outputs)
+
+
+def softmax(x):
+    assert len(x.shape) == 1
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
 
 
 print('Check LAMBADA...')
@@ -55,16 +41,16 @@ xcnt = 0
 xacc = 0
 for d in todo:
     model.reset_states()
-    src = PAD_SEQ + sampler.encode(d[0])
+    src = sampler.encode(d[0])
     dst = sampler.encode(d[1])
 
     logits = 0
     correct = True
     out = forward_with_full_output(model, src+dst)
     for i in range(len(dst)):
-        probs = F.softmax(out[len(src)-1+i,:], dim=-1)
+        probs = softmax(out[len(src)-1+i])
         logits += math.log(probs[dst[i]])
-        if torch.argmax(probs).item() != dst[i]:
+        if np.argmax(probs).item() != dst[i]:
             correct = False
 
     xcnt += 1
